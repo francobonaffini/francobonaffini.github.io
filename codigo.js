7a23b385242bd063269478e20e556820507e2747
francobonaffini <112186574+francobonaffini@users.noreply.github.com>
2022-08-26 20:37:33 -0300

    commit 10

---
+++ codigo.js
@@ -0,0 +1,11 @@
+
+const boton = document.querySelector(".nav2");
+
+
+
+boton.addEventListener("click" , ()=> {
+
+    let liContainer = document.querySelector(".nav3")
+    
+    liContainer.classList.toggle("nav4")
+})
